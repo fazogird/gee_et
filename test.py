a8e93f0e994bec09c859ea2acf61900faeb0fd13
FAZOGIR DOSTON <fazogird@gmail.com>
2025-02-13 23:42:50 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import geemap
 
 # Streamlit ilovasi sarlavhasi
 st.title("Interaktiv Polygon Chizish va NDVI Vizualizatsiyasi")
 
-# Geemap xaritasi
-Map = geemap.Map(center=[40.7128, -74.0060], zoom=10)
+
